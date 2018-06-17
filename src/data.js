67661d8cca6106d60d01af09b1d273ac1cec9f78
FiorellaEffio <fiorella.effio@unmsm.edu.pe>
2018-06-17 18:09:17 -0500

    mostrando cohorts en lista desplegable

--- src/data.js
+++ src/data.js
@@ -1,20 +1,43 @@
-const dataRequest = new XMLHttpRequest();
-dataRequest.open('GET', "../data/cohorts/lim-2018-03-pre-core-pw/users.json");
-dataRequest.onload = function () {
-  if(dataRequest.status>=200 && dataRequest.status<400) {
-  var data = JSON.parse(dataRequest.responseText);
-  let arrayStudents = [];
-  for(i=0;i<735;i++) {
-      arrayStudents[i] = (data[i]);
-  }
+const cohortsLaboratoria = '../data/cohorts.json';
+const cohortPrincipal = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
+const progressOfCohortPrincipal = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
+var arrayStudents = [];
+fetch(cohortsLaboratoria).then((response)=>{
+    if (response.status !== 200) {
+        //Código cuando recibimos una respuesta corréctamente del servidor
+        console.log("Error");
+        // Solo si es que sabemos que la respuesta es JSON, o fallará
+    } else{
+        //Código en caso de que nos respondan con algún error
+        return response.json();
+    }
+}).then((respuestaJson)=>{
+    //Código que usa el JSON
+    console.log(respuestaJson);
+    for (var i = 0; i < respuestaJson.length; i++) {
+      arrayStudents[i] = respuestaJson[i];
+    }
+    console.log(arrayStudents);
+    beginHTML(arrayStudents);
+
+}).catch((error)=>{
+    /* Código en caso de que la llamada falle
+     * Como cuando el usuario NO tiene internet, o se haya cortado la
+     * comunicación.
+     */
+     console.log("No hay resultados :(");
+});
+function beginHTML(arrayStudents) {
   console.log(arrayStudents);
-  } else {
-    dataRequest.onerror = handleError;
-  }
-};
-dataRequest.send();
-console.log(dataRequest);
-console.log(arrayStudents);
-function handleError() {
-  console.log('Se ha presentado un error');
+  arrayStudents.forEach(function(element) {
+  console.log(element);
+  let nameOfCohort = document.createElement('option');
+  nameOfCohort.value = element.id;
+  nameOfCohort.innerText = element.id;
+  cohortsOptions.appendChild(nameOfCohort);
+  });
+  // for (var i = 0; i < arrayStudents.length; i++) {
+  //   console.log(arrayStudents[i].id);
+  //   (document.getElementById('cohortsOptions')).appendChild("<option>" + arrayStudents[i].id + "</option>");
+  // }
 }
