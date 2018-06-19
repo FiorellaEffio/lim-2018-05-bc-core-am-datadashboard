6315a912d1ac246b0b02fd837e52bcf20914ac01
Aweba13 <fiorella.effio@unmsm.edu.pe>
2018-06-19 12:37:39 -0500

    mostrando 1 usuario objeto con datos de cohorts.json y users.json

--- src/data.js
+++ src/data.js
@@ -1,74 +1,85 @@
-const cohortsLaboratoria = '../data/cohorts.json';
-var users, progress;
-var chargeAll = document.getElementById('buttonCharge');
-chargeAll.addEventListener("click", chargeData)
-function chargeData() {
-  users = bringData('../data/cohorts/lim-2018-03-pre-core-pw/users.json');
-  progress = bringData('../data/cohorts.json');
-}
-
-function bringData(jsonFile) {
-  fetch(jsonFile).then((response)=>{
-      if (response.status !== 200) {
-          //Código cuando recibimos una respuesta corréctamente del servidor
-          console.log("Error");
-          // Solo si es que sabemos que la respuesta es JSON, o fallará
-      } else{
-          //Código en caso de que nos respondan con algún error
-         return response.json();
-         console.log(response.json);
-      }
-  }).then((answerJson)=>{
-      //Código que usa el JSON
-      let rawData = [];
-      for (var i = 0; i < answerJson.length; i++) {
-        rawData[i] = answerJson[i];
-      }
-      printingList(rawData);
-
-  }).catch((error)=>{
-       console.log("No hay resultados :(");
-  });
-}
-
-function printingList(rawData) {
-  console.log(rawData);
-  rawData.forEach(function(element) {
-  let nameOfElement = document.createElement('p');
-  nameOfElement.innerText = element.id;
-  studentsOptions.appendChild(nameOfElement);
-  });
-  return rawData;
-}
-console.log(users);
-
-
-
-//Data Dashboard
-// 1) computeUsersStats(users, progress, courses)
 window.computeUsersStats = (users, progress, courses) => {
   // Esta función es la responsable de "crear" la lista de usuarios (estudiantes) que vamos a "pintar" en la pantalla. La idea es "recorrer" el arreglo de usuarios (users) y calcular los indicadores necesarios de progreso para cada uno. La función debe devolver un nuevo arreglo de usuarios donde a cada objeto de usuario se le debe agregar una propiedad con el nombre stats con las estadísticas calculadas.
+  // progress: Objeto de progreso en bruto. Contiene una llave para cada usuario (uid) con un objeto que contiene el progreso del usuario para cada curso.
+  // courses: Arreglo de strings con los ids de los cursos del cohort en cuestión. Esta data se puede extraer de la propiedad coursesIndex de los objetos que representan los cohorts.
+
+  var myList = [];
+  var usersWithStats = {
+    stats = {
+      percent: 0,//porcentaje total respecto a cursos totales del cohort
+      exercises = {
+        total: 0,//total de ejercicios autocorregidos
+        completed: 0,//autocorregidos completados
+        percent: 0//porcentaje de ejercicios autocorregidos autocompletados
+      },
+      reads = {
+        total:0,//total de lecturas presentes
+        completed: 0, //lecturas completadas
+        percent: 0 //porcentaje de lecturas
+      },
+      quizzes = {
+        total: 0, //total quizzes presentes
+        completed: 0, // quizzes autocompletados
+        percent: 0, //porcentaje de quizzes completados
+        scoreSum: 0, //suma de puntuaciones de los _quizzes_ completados
+        scoreAvg: 0 //promedio de puntuaciones en quizzes completados
+      }
+    }
+  }
+  return myList;
 }
-//
-// Argumentos
-// users: Arreglo de objetos obtenido de la data en bruto.
-// progress: Objeto de progreso en bruto. Contiene una llave para cada usuario (uid) con un objeto que contiene el progreso del usuario para cada curso.
-// courses: Arreglo de strings con los ids de los cursos del cohort en cuestión. Esta data se puede extraer de la propiedad coursesIndex de los objetos que representan los cohorts.
-// Valor de retorno
-// Un arreglo de objetos usersWithStats con la propiedad stats, la cual debe ser un objeto con las siguientes propiedades:
-//
-// percent: Número entero entre 0 y 100 que indica el porcentaje de completitud general del usuario con respecto a todos los cursos asignados a su cohort.
-// exercises: Objeto con tres propiedades:
-            // total: Número total de ejercicios autocorregidos presentes en cursos del cohort.
-            // completed: Número de ejercicios autocorregidos completados por el usuario.
-            // percent: Porcentaje de ejercicios autocorregidos completados.
-// reads: Objeto con tres propiedades:
-            // total: Número total de lecturas presentes en cursos del cohort.
-            // completed: Número de lecturas completadas por el usuario.
-            // percent: Porcentaje de lecturas completadas.
-// quizzes: Objeto con cinco propiedades:
-            // total: Número total de quizzes presentes en cursos del cohort.
-            // completed: Número de quizzes completadas por el usuario.
-            // percent: Porcentaje de quizzes completadas.
-            // scoreSum: Suma de todas las puntuaciones (score) de los quizzes completados.
-            // scoreAvg: Promedio de puntuaciones en quizzes completados.
+
+window.sortUsers = (users, orderBy, orderDirection) => {
+  // La función `sortUsers()` se encarga de _ordenar_ la lista de usuarios creada con
+  // `computeUsersStats()` en base a `orderBy` y `orderDirection`.
+  // * `users`: Arreglo de objetos creado con `computeUsersStats()`.
+  // * `orderBy`: String que indica el criterio de ordenado. Debe permitir ordenar
+  //   por nombre, porcentaje de completitud total, porcentaje de ejercicios
+  //   autocorregidos completados, porcentaje de quizzes completados, puntuación
+  //   promedio en quizzes completados, y porcentaje de lecturas completadas.
+  // * `orderDirection`: La dirección en la que queremos ordenar. Posibles valores:
+  //   `ASC` y `DESC` (ascendiente y descendiente).
+  return myListByOrder;//arreglo de usuarios ordenados
+
+}
+
+window.filterUsers = (users, search) => {
+  // * `users`: Arreglo de objetos creado con `computeUsersStats()`.
+  // * `search`: String de búsqueda.
+  return myListFiltered;//lista de usuarios con coincidencia en search
+}
+
+window.processCohortData = (options) => {
+  // Esta función es la que deberíamos usar al seleccionar un cohort y cada vez que
+  // el usuario cambia los criterios de ordenado y filtrado en la interfaz. Esta
+  // función debe invocar internamente a `computeUsersStats()`, `sortUsers()` y
+  // `filterUsers()`.
+  var options = {
+    cohort: ,//objeto cohort de la lista de cohorts
+    cohortData = {
+      users: [],
+      progress = {
+
+      }
+    },
+    orderBy: ,//string del sortUsers
+    orderDirection: ,//string del sortUsers
+    search://string del filterUsers
+  }
+  var myListOrderAndFiltered = {
+    stats: //ver computeUsersStats
+  }
+  return myListOrderAndFiltered;
+}
+
+            // 1. Permitir al usuario seleccionar un cohort de una lista de cohorts.
+            // 2. Al seleccionar un cohort:
+            //    - Listar las estudiantes de ese cohort
+            //    - Para cada estudiante:
+            //      + Calcular porcentaje de completitud de todos los _cursos_.
+            //      + Calcular grado de completitud de _lecturas_, _ejercicios autocorregidos_,
+            //        y _quizzes_.
+            //    - Ordenar estudiantes por completitud _general_ (porcentaje consumido/completado
+            //      de todos los cursos del cohort en cuestión), de _lecturas_, _ejercicios
+            //      autocorregidos_ y _quizzes_.
+            //    - Filtrar/buscar estudiantes por nombre.
